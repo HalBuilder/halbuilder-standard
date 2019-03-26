41aadad418c888fff15c9cd470c5dcfaf12b7b0c
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2019-03-26 08:56:36 +0000

    jdk 9 build

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.theoryinpractise.halbuilder.standard {
+    exports com.theoryinpractise.halbuilder.standard;
+    requires transitive com.theoryinpractise.halbuilder.api;
+    requires transitive com.theoryinpractise.halbuilder.core;
+    requires transitive com.theoryinpractise.halbuilder.json;
+    requires transitive com.theoryinpractise.halbuilder.xml;
+}
